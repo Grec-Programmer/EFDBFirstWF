eead10389716e7d8edd95479faf9709916b2c7eb
Master <daniil.grechko.00@mail.ru>
2023-04-25 21:22:45 +0300

    Создание Справочника машин

---
+++ ORMEFWF/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ORMEFWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBoxName = new System.Windows.Forms.TextBox();
+            this.tBoxPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddCar = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tBoxName
+            // 
+            this.tBoxName.Location = new System.Drawing.Point(281, 10);
+            this.tBoxName.Name = "tBoxName";
+            this.tBoxName.Size = new System.Drawing.Size(100, 20);
+            this.tBoxName.TabIndex = 0;
+            // 
+            // tBoxPrice
+            // 
+            this.tBoxPrice.Location = new System.Drawing.Point(281, 38);
+            this.tBoxPrice.Name = "tBoxPrice";
+            this.tBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.tBoxPrice.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Марка машины";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Цена";
+            // 
+            // btnAddCar
+            // 
+            this.btnAddCar.Location = new System.Drawing.Point(193, 64);
+            this.btnAddCar.Name = "btnAddCar";
+            this.btnAddCar.Size = new System.Drawing.Size(197, 23);
+            this.btnAddCar.TabIndex = 5;
+            this.btnAddCar.Text = "Добавить машину";
+            this.btnAddCar.UseVisualStyleBackColor = true;
+            this.btnAddCar.Click += new System.EventHandler(this.btnAddCar_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(13, 10);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(174, 251);
+            this.listBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Удалить Машину из базы";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 268);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.btnAddCar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tBoxPrice);
+            this.Controls.Add(this.tBoxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBoxName;
+        private System.Windows.Forms.TextBox tBoxPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddCar;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ORMEFWF/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ORMEFWF
+{
+    public partial class Form1 : Form
+    {
+        void ShowDB()
+        {
+            using (AutoDealerEntities db = new AutoDealerEntities())
+            {
+                var query = (from s in db.Avtoes select s).ToList();
+                listBox.Items.Clear();
+                foreach (var item in query)
+                {
+
+                    listBox.Items.Add($"{item.Id}.{item.Name} Prcie: {Convert.ToDecimal(item.Price).ToString("#,#", CultureInfo.InvariantCulture)}");
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            ShowDB();
+        }
+
+        private void btnAddCar_Click(object sender, EventArgs e)
+        {
+            if (tBoxName.Text != "")
+            {
+                    decimal p = 0;
+                if (tBoxPrice.Text != "")
+                    p = Convert.ToDecimal(tBoxPrice.Text);
+                using (AutoDealerEntities db = new AutoDealerEntities())
+                {
+                    Avto avto = new Avto { Name = tBoxName.Text, Price = p };
+                    db.Avtoes.Add(avto);
+                    db.SaveChanges();
+                        var query = (from s in db.Avtoes select s).ToList();
+                        listBox.Items.Clear();
+                        foreach (var item in query)
+                        {
+
+                            listBox.Items.Add($"{item.Id}.{item.Name} Prcie: {Convert.ToDecimal(item.Price).ToString("#,#", CultureInfo.InvariantCulture)}");
+                        }
+                }            
+            }
+        }
+    }
+}
